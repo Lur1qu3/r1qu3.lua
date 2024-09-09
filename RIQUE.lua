0d0372de163963cf316bd8d79201f2fb3b201050
Lur1qu3 <mcmclhmm1@gmail.com>
2024-09-08 23:40:30 -0300

    Update RIQUE.lua

--- RIQUE.lua
+++ RIQUE.lua
@@ -74,6 +74,25 @@ macro(3000, "Comprar AOL", function()
 end)
 
 
+macro(100, "Invit PT", function()
+for i,v in ipairs (getSpectators(posz())) do
+    if v ~= player and v:isPlayer() and v:getShield() == 0 and v:getEmblem() == 1 then
+        g_game.partyInvite(v:getId())
+    end
+end
+end)
+
+
+
+macro(100, "Accept PT", function()
+for i,v in ipairs (getSpectators(posz())) do
+    if v ~= player and v:isPlayer() and v:getShield() == 1 and v:getEmblem() == 1 then
+        g_game.partyJoin(v:getId())
+    end
+end
+end)
+
+
 followName = "autofollow"
 if not storage[followName] then storage[followName] = { player = 'name'} end
 local toFollowPos = {}
